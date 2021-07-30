7a7a993bfefd796bf76f8e2b3814b31b6c51d5c6
roman <inyellowbus@gmail.com>
2021-07-30 16:12:18 +0300

    feat: rename store keys

--- src/DemiurgeStore.sol
+++ src/DemiurgeStore.sol
@@ -8,23 +8,27 @@ import './interfaces/IDemiurgeStoreCallback.sol';
 
 contract DemiurgeStore is Base {
 
-    mapping(uint8 => address) public addrs;
-    mapping(uint8 => TvmCell) public codes;
+    mapping(uint8 => address) public _addrs;
+    mapping(uint8 => TvmCell) public _codes;
 
     function setPadawanCode(TvmCell code) public signed {
-        codes[uint8(ContractType.Padawan)] = code;
+        _codes[uint8(ContractType.Padawan)] = code;
     }
     function setProposalCode(TvmCell code) public signed {
-        codes[uint8(ContractType.Proposal)] = code;
+        _codes[uint8(ContractType.Proposal)] = code;
     }
 
     function setDensRootAddr(address addr) public signed {
         require(addr != address(0));
-        addrs[uint8(ContractAddr.DensRoot)] = addr;
+        _addrs[uint8(ContractAddr.DensRoot)] = addr;
     }
     function setTokenRootAddr(address addr) public signed {
         require(addr != address(0));
-        addrs[uint8(ContractAddr.TokenRoot)] = addr;
+        _addrs[uint8(ContractAddr.TokenRoot)] = addr;
+    }
+    function setFaucetTokenWalletAddr(address addr) public signed {
+        require(addr != address(0));
+        _addrs[uint8(ContractAddr.FaucetTokenWallet)] = addr;
     }
 
     /*
@@ -32,12 +36,12 @@ contract DemiurgeStore is Base {
      */
 
     function queryCode(ContractType kind) public view {
-        TvmCell code = codes[uint8(kind)];
+        TvmCell code = _codes[uint8(kind)];
         IDemiurgeStoreCallback(msg.sender).updateCode{value: 0, flag: 64, bounce: false}(kind, code);
     }
 
     function queryAddr(ContractAddr kind) public view {
-        address addr = addrs[uint8(kind)];
+        address addr = _addrs[uint8(kind)];
         IDemiurgeStoreCallback(msg.sender).updateAddr{value: 0, flag: 64, bounce: false}(kind, addr);
     }
 }
